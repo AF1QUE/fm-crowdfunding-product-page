6c6fbb663c8040e4d1fdf1cb22a31a58586a854a
AF1QUE <afique.dev@gmail.com>
2022-04-05 00:40:31 +0600

    Initiliazed with necessary files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      moderateCyan: "hsl(176, 50%, 47%)",
+      darkCyan: "hsl(176, 72%, 28%)",
+      black: "hsl(0, 0%, 0%)",
+      darkGray: "hsl(0, 0%, 48%)",
+    },
+  },
+  plugins: [],
+};
